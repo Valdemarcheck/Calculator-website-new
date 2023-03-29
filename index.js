815c47619aac8e139e39c2450a688e4b14616c80
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-03-29 17:35:37 +0500

    Add experimental expression evaluation

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const clearOneBtn = document.querySelector(".button.clear-one");
 const write = document.querySelector(".write");
 const MAX_SIGNS = 40;
 
-const MATH_OPERANDS = Array.from(basicOperationBtns).map((btn) => {
-  let span = btn.querySelector("span");
-  return span.textContent;
-});
+const MATH_OPERANDS = ["√", "^", "*", "/", "+", "-"];
 
 const BASIC = "basic";
 const IN_FUNCTION = "in_function";
@@ -23,6 +20,7 @@ clearOneBtn.addEventListener("click", () => {
     write.textContent = write.textContent.slice(0, secondLastIndex);
   }
 });
+
 clearAllBtn.addEventListener("click", () => (write.textContent = ""));
 
 numberBtns.forEach((btn) =>
@@ -31,6 +29,8 @@ numberBtns.forEach((btn) =>
 basicOperationBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => appendCharacter(e))
 );
+
+equalsBtn.addEventListener("click", evaluateExpression);
 
 function appendCharacter(e) {
   if (isStringTooLong()) return;
@@ -42,6 +42,39 @@ function appendCharacter(e) {
     let character = e.target.textContent;
     write.textContent += character;
   }
+}
+
+function evaluateExpression() {
+  if (isStringValid()) {
+    if (mode === BASIC) {
+      performOperations(write.textContent);
+    }
+  } else {
+    console.log("invalid!");
+  }
+}
+
+function performOperations(expression) {
+  for (let sign of MATH_OPERANDS) {
+    let pattern = new RegExp(`(?=([\\d\\.]+\\${sign}[\\d\\.]+))`, "g");
+    let allMatches = [...expression.matchAll(pattern)];
+    console.log(allMatches);
+    let evaluatedMatches = allMatches.reduce((solvedMatches, match) => {
+      let expression = match[1];
+      let counterparts = expression.split(sign);
+      let result = operate(counterparts[0], counterparts[1], sign);
+      solvedMatches[expression] = result;
+    }, {});
+    console.log(evaluatedMatches);
+  }
+}
+
+function isStringValid() {
+  return !(
+    isStringEmpty() ||
+    isLastCharacterDot() ||
+    isLastCharacterMathOperand()
+  );
 }
 
 function isStringEmpty() {
@@ -59,4 +92,29 @@ function isLastCharacterDot() {
 
 function isStringTooLong() {
   return write.textContent.length >= MAX_SIGNS;
+}
+
+function operate(a, b, sign) {
+  a = +a;
+  b = +b;
+  let result;
+  switch (sign) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    case "^":
+      result = a ** b;
+    case "√":
+      result = Math.pow(a, 1 / b);
+  }
+  return result.toFixed(5);
 }
